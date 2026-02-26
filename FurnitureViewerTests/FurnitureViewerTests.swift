68a9ae74d6f6915d62ca1fcf40ca9cdc3b4bbd35
philippm01 <philipp@gmail.com>
2026-02-25 18:04:52 -0800

    Initial Commit

---
+++ FurnitureViewerTests/FurnitureViewerTests.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureViewerTests.swift
+//  FurnitureViewerTests
+//
+//  Created by Philipp on 2/25/26.
+//
+
+import Testing
+@testable import FurnitureViewer
+
+struct FurnitureViewerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
